6b41993fcede336e285dfcaa19e6f7493dde139a
Pandey <2370817@cognizant.com>
2025-06-14 05:05:01 -0400

    restart project

--- PaymentService/src/main/java/com/payment/PaymentServiceApplication.java
+++ PaymentService/src/main/java/com/payment/PaymentServiceApplication.java
@@ -2,12 +2,13 @@ package com.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class PaymentServiceApplication {
+@EnableFeignClients
+ class PaymentServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentServiceApplication.class, args);
